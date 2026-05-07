771ff20a42a78219b4c69289f9b0a70e92f459a4
Rayan MRABET <112360249+RayanMRABET@users.noreply.github.com>
2026-05-07 12:56:34 +0100

    Prioritize XML channel mapping over DB channel info (#33)
    * Prioritize XML channel mapping over DB channel info
    
    * Clear any DbChannelInfo mappings added before XML was ready
    
    * replace find_package with FetchContent for tinyxml2 and ZLIB
    
    * replace find_package with FetchContent for  args
    
    * correct target names for args and ZLIB after FetchContent
    
    * revert
    
    Removed FetchContent for zlib and added find_package instead.
    
    * revert
    
    * Update src/channels.cpp
    
    apply suggestion
    
    Co-authored-by: Ayoub Kaanich <kayoub5@live.com>
    
    * revert changes in configure_xml_channels
    
    ---------
    
    Co-authored-by: Rayan MRABET <rayan.mrabet@technica-engineering.de>
    Co-authored-by: Ayoub Kaanich <kayoub5@live.com>

--- src/channels.cpp
+++ src/channels.cpp
@@ -144,10 +144,10 @@ void configure_xml_channels(pcapng_exporter::PcapngExporter* exporter, AppText* 
 }
 
 void configure_channels(pcapng_exporter::PcapngExporter* exporter, AppText* obj) {
-	if (obj->source == AppText::Source::DbChannelInfo) {
-		configure_db_channel(exporter, obj);
-	}
 	if (obj->source == AppText::Source::MetaData) {
 		configure_xml_channels(exporter, obj);
+	}
+	if (obj->source == AppText::Source::DbChannelInfo) {
+		configure_db_channel(exporter, obj);
 	}
 }
